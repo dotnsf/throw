ac4c843821dbc9771295eec46454f62328b14dd8
K.Kimura <dotnsf@gmail.com>
2019-06-10 11:13:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Throw
+
+## Overview
+
+
+
+## Pre-requisite
+
+- Public application server for Node.js
+
+    - I would describe followings as you use IBM Cloud for this environment.
+
+
+## Pre-requisite for IBM Cloud user
+
+- Node.js runtime
+
+
+## Copyright
+
+2019 [K.Kimura @ Juge.Me](https://github.com/dotnsf) all rights reserved.
---
+++ app.js
@@ -0,0 +1,191 @@
+//. app.js
+
+var express = require( 'express' ),
+    basicAuth = require( 'basic-auth-connect' ),
+    cfenv = require( 'cfenv' ),
+    multer = require( 'multer' ),
+    bodyParser = require( 'body-parser' ),
+    fs = require( 'fs' ),
+    ejs = require( 'ejs' ),
+    uuidv1 = require( 'uuid/v1' ),
+    app = express();
+
+var appEnv = cfenv.getAppEnv();
+
+app.use( multer( { dest: './tmp/' } ).single( 'image' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+app.use( express.Router() );
+app.use( express.static( __dirname + '/public' ) );
+
+app.set( 'views', __dirname + '/views' );
+app.set( 'view engine', 'ejs' );
+
+app.get( '/', function( req, res ){
+  res.render( 'throw', {} );
+});
+
+app.post( '/image', function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  if( db ){
+    var imgpath = req.file.path;
+    var imgtype = req.file.mimetype;
+    //var imgsize = req.file.size;
+    var ext = imgtype.split( "/" )[1];
+    var imgfilename = req.file.filename;
+    var filename = req.file.originalname;
+
+    var image_id = uuidv1();
+    var img = fs.readFileSync( imgpath );
+    var img64 = new Buffer( img ).toString( 'base64' );
+
+    var params = {
+      _id: image_id,
+      filename: filename,
+      type: 'image',
+      timestamp: ( new Date() ).getTime(),
+      name: req.body.name,
+      _attachments: {
+        image: {
+          content_type: imgtype,
+          data: img64
+        }
+      }
+    };
+    db.insert( params, image_id, function( err, body, header ){
+      if( err ){
+        console.log( err );
+        var p = JSON.stringify( { status: false, error: err }, null, 2 );
+        res.status( 400 );
+        res.write( p );
+        res.end();
+      }else{
+        var p = JSON.stringify( { status: true, id: image_id, body: body }, null, 2 );
+        res.write( p );
+        res.end();
+      }
+      fs.unlink( imgpath, function( err ){} );
+    });
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'db is not initialized.' } ) );
+    res.end();
+  }
+});
+
+app.get( '/image', function( req, res ){
+  if( db ){
+    var image_id = req.query.id;
+    db.attachment.get( image_id, 'image', function( err1, body1 ){
+      res.contentType( 'image/png' );
+      res.end( body1, 'binary' );
+    });
+  }else{
+    res.contentType( 'application/json; charset=utf-8' );
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'db is not initialized.' } ) );
+    res.end();
+  }
+});
+
+app.delete( '/image', function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  if( db ){
+    var id = req.query.id;
+
+    //. Cloudant から削除
+    db.get( id, null, function( err1, body1, header1 ){
+      if( err1 ){
+        err1.image_id = "error-1";
+        res.status( 400 );
+        res.write( JSON.stringify( { status: false, error: err1 } ) );
+        res.end();
+      }
+
+      var rev = body1._rev;
+      db.destroy( id, rev, function( err2, body2, header2 ){
+        if( err2 ){
+          err2.image_id = "error-2";
+          res.status( 400 );
+          res.write( JSON.stringify( { status: false, error: err2 } ) );
+          res.end();
+        }
+
+        body2.image_id = id;
+        res.write( JSON.stringify( { status: true, body: body2 } ) );
+        res.end();
+      });
+    });
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'db is not initialized.' } ) );
+    res.end();
+  }
+});
+
+
+app.get( '/images', function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var limit = req.query.limit ? parseInt( req.query.limit ) : 0;
+  var offset = req.query.offset ? parseInt( req.query.offset ) : 0;
+
+  if( db ){
+    db.list( { include_docs: true }, function( err, body ){
+      if( err ){
+        res.status( 400 );
+        res.write( JSON.stringify( { status: false, message: err }, 2, null ) );
+        res.end();
+      }else{
+        var total = body.total_rows;
+        var images = [];
+        body.rows.forEach( function( doc ){
+          var _doc = JSON.parse(JSON.stringify(doc.doc));
+          if( _doc._id.indexOf( '_' ) !== 0 && _doc.type && _doc.type == 'image' ){
+            images.push( _doc );
+          }
+        });
+
+        if( offset || limit ){
+          if( offset + limit > total ){
+            images = images.slice( offset );
+          }else{
+            images = images.slice( offset, offset + limit );
+          }
+        }
+
+        var result = { status: true, total: total, limit: limit, offset: offset, images: images };
+        res.write( JSON.stringify( result, 2, null ) );
+        res.end();
+      }
+    });
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, message: 'db is failed to initialize.' }, 2, null ) );
+    res.end();
+  }
+});
+
+
+function timestamp2datetime( ts ){
+  if( ts ){
+    var dt = new Date( ts );
+    var yyyy = dt.getFullYear();
+    var mm = dt.getMonth() + 1;
+    var dd = dt.getDate();
+    var hh = dt.getHours();
+    var nn = dt.getMinutes();
+    var ss = dt.getSeconds();
+    var datetime = yyyy + '-' + ( mm < 10 ? '0' : '' ) + mm + '-' + ( dd < 10 ? '0' : '' ) + dd
+      + ' ' + ( hh < 10 ? '0' : '' ) + hh + ':' + ( nn < 10 ? '0' : '' ) + nn + ':' + ( ss < 10 ? '0' : '' ) + ss;
+    return datetime;
+  }else{
+    return "";
+  }
+}
+
+
+app.listen( appEnv.port );
+console.log( "server stating on " + appEnv.port + " ..." );
